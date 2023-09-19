3ae3040cdadbae2ad4405a30e985ad0a12dd8b7b
umutibrahimtoksoy <ibrahim.toksoy@wsrlife.com>
2023-09-19 17:00:59 +0300

    Add project files.

---
+++ Core/ECommerceDomain/Models/Common/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerceDomain.Models.Common
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ Core/ECommerceDomain/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿using ECommerceDomain.Models.Common;
+
+namespace ECommerceDomain.Models
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public long Price { get; set; }
+    }
+}
