8229c4a335fc3e83d6cb859092354cdfed9c5f52
umutibrahimtoksoy <ibrahim.toksoy@wsrlife.com>
2023-11-07 16:36:47 +0300

    Added new columns for BaseEntity class

--- Core/ECommerceDomain/Models/Common/BaseEntity.cs
+++ Core/ECommerceDomain/Models/Common/BaseEntity.cs
@@ -3,7 +3,11 @@
     public class BaseEntity
     {
         public int Id { get; set; }
-
         public DateTime CreatedDate { get; set; }
+        public int CreatedUserId { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+        public int? UpdatedUserId { get; set; }
+        public DateTime? DeletedDate { get; set; }
+        public int? DeletedUserId { get; set; }
     }
 }
---
+++ Infrastructure/ECommercePersistence/Migrations/20231107133527_NewColumnsAddedForBaseEntity.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommercePersistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewColumnsAddedForBaseEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedUserId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedDate",
+                table: "Products",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DeletedUserId",
+                table: "Products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Products",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UpdatedUserId",
+                table: "Products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedUserId",
+                table: "Orders",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DeletedUserId",
+                table: "Orders",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UpdatedUserId",
+                table: "Orders",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedUserId",
+                table: "Customers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedDate",
+                table: "Customers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DeletedUserId",
+                table: "Customers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Customers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UpdatedUserId",
+                table: "Customers",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedUserId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedDate",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedUserId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedUserId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedUserId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedDate",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedUserId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedUserId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedUserId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedDate",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedUserId",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedUserId",
+                table: "Customers");
+        }
+    }
+}
